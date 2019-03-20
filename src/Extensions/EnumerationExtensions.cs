5d57489ee7f7ecd4a64ecf18e66e09fcc614b55a
restlessmedia <phillycooper@hotmail.com>
2019-03-20 09:43:45 +0000

    Added enum extensions

---
+++ src/Extensions/EnumerationExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace System
+{
+  /// <summary>
+  /// Extensions for strongly typed enum
+  /// </summary>
+  public static class EnumerationExtensions
+  {
+    /// <summary>
+    /// If applicable, returns the value of description attribute for an enum value
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public static string GetDescription(this Enum value)
+    {
+      FieldInfo field = value.GetType().GetField(value.ToString());
+      DescriptionAttribute attribute = Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) as DescriptionAttribute;
+      return attribute == null ? value.ToString() : attribute.Description;
+    }
+  }
+}
