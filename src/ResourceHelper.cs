4fea2a27c76c9d65d60292733bc87f56ba872fb9
restlessmedia <phillycooper@hotmail.com>
2019-03-20 18:19:58 +0000

    Added resource helper

---
+++ src/ResourceHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace restlessmedia.Module
+{
+  public static class ResourceHelper
+  {
+    public static bool Exists(Assembly assembly, string resource)
+    {
+      return assembly.GetManifestResourceNames().Contains(resource);
+    }
+
+    public static bool Exists(Assembly assembly, string resource, out Stream stream)
+    {
+      if (string.IsNullOrWhiteSpace(resource))
+      {
+        throw new ArgumentNullException(nameof(resource));
+      }
+
+      stream = assembly.GetManifestResourceStream(resource);
+
+      return stream != null;
+    }
+
+    public static Stream GetResourceStream(Assembly assembly, string resource)
+    {
+      Stream stream;
+
+      if (!Exists(assembly, resource, out stream))
+      {
+        throw new FileNotFoundException($"No resource matching '{resource}' could be found in assembly '{assembly.FullName}'.");
+      }
+
+      return stream;
+    }
+  }
+}
