2b738f8a2993fe89c1bf6aef73561adfc8223be5
restlessmedia <phillycooper@hotmail.com>
2019-03-26 13:32:41 +0000

    Autofac extensions

---
+++ src/Extensions/AutofacExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Autofac.Configuration;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Autofac
+{
+  public static class AutofacExtensions
+  {
+    public static void AddJsonFile(this ContainerBuilder builder, string jsonFile)
+    {
+      if (string.IsNullOrEmpty(jsonFile))
+      {
+        throw new ArgumentNullException(nameof(jsonFile), $"{nameof(jsonFile)} cannot be null or empty for configuring Autofac container.");
+      }
+
+      ConfigurationBuilder configBuilder = new ConfigurationBuilder();
+      builder.AddJsonFile(jsonFile);
+      ConfigurationModule module = new ConfigurationModule(configBuilder.Build());
+      builder.RegisterModule(module);
+    }
+  }
+}
