474d2921f43d0218a2e5ad49e97684025e3e3126
restlessmedia <phillycooper@hotmail.com>
2019-04-01 09:24:54 +0100

    Added enumerable helper

---
+++ restlessmedia.Module.UnitTest/EnumerableHelperTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace restlessmedia.Module.UnitTest
+{
+  [TestClass]
+  public class EnumerableHelperTests
+  {
+    [TestMethod]
+    public void Range_returns_number_range()
+    {
+      EnumerableHelper.Range(1, 10, 1).MustContain(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+      EnumerableHelper.Range(1, 10, 5).MustContain(1, 6);
+      EnumerableHelper.Range(2, 10, 2).MustContain(2, 4, 6, 8, 10);
+    }
+  }
+}
---
+++ src/EnumerableHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace restlessmedia.Module
+{
+  public class EnumerableHelper
+  {
+    public static IEnumerable<int> Range(int from, int to, int step)
+    {
+      for (int current = from; current < to; current = current + step)
+      {
+        yield return current;
+      }
+    }
+  }
+}
