7f9ecee719f31b1d1e4ca61801c7270c0d4da939
restlessmedia <phillycooper@hotmail.com>
2019-04-05 12:21:58 +0100

    Fixing test

--- restlessmedia.Module.UnitTest/Extensions/EnumerationExtensionsTests.cs
+++ restlessmedia.Module.UnitTest/Extensions/EnumerationExtensionsTests.cs
@@ -6,23 +6,6 @@ namespace restlessmedia.Module.UnitTest.Extensions
   [TestClass]
   public class EnumerationExtensionsTests
   {
-    [TestMethod]
-    public void SetFlag()
-    {
-      Test test = Test.None;
-      test.SetFlag(Test.One);
-
-      test.SetFlag(Test.Two);
-    }
-
-    [Flags]
-    private enum Test
-    {
-      None = 0,
-      One = 1,
-      Two = 2,
-      Four = 4,
-      All = 7,
-    }
+    
   }
 }
