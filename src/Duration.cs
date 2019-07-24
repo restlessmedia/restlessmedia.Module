bd68f8780ed1c210f2bb01ad150b86e124917754
Philip Cooper <phillycooper@hotmail.com>
2019-07-24 20:14:27 +0100

    Added duration

---
+++ src/Duration.cs
@@ -0,0 +1,20 @@
+﻿namespace restlessmedia.Module
+{
+  /// <summary>
+  /// Constant values for time.
+  /// </summary>
+  public static class Duration
+  {
+    public const int WeekInSeconds = 604800;
+
+    public const int TenMinutesInSeconds = 600;
+
+    public const int HourInSeconds = 3600;
+
+    public const int TwoHoursInSeconds = 7200;
+
+    public const int HalfADayInSeconds = 43200;
+
+    public const int DayInSeconds = 86400;
+  }
+}
