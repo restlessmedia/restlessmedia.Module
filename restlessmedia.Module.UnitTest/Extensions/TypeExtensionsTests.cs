9aa345a0ec1e1266fc2d61cfa89c8d580ff8fcd7
Philip Cooper <phillycooper@hotmail.com>
2019-08-15 17:55:33 +0100

    Added test

---
+++ restlessmedia.Module.UnitTest/Extensions/TypeExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using restlessmedia.Module.Extensions;
+using restlessmedia.Test;
+using System.ComponentModel;
+using System.Linq;
+using Xunit;
+
+namespace restlessmedia.Module.UnitTest.Extensions
+{
+  public class TypeExtensionsTests
+  {
+    [Fact]
+    public void GetMembers_finds_members_with_attribute()
+    {
+      TypeExtensions.GetMembers<ReadOnlyAttribute>(typeof(TestClass), x => x != null && x.IsReadOnly).Count().MustBe(1);
+    }
+
+    private class TestClass
+    {
+      [ReadOnly(true)]
+      public void Foo() { }
+    }
+  }
+}
