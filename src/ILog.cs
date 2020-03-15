6d4abf9650c8214e1715f5e322811c837c544e62
Philip Cooper <phillycooper@hotmail.com>
2020-03-15 20:29:14 +0000

    Adding log and langversion

---
+++ src/ILog.cs
@@ -0,0 +1,14 @@
+﻿namespace restlessmedia.Module
+{
+  /// <summary>
+  /// Logging abstraction
+  /// </summary>
+  public interface ILog
+  {
+    void Info(string message);
+
+    void Warning(string message);
+
+    void Error(string message);
+  }
+}
