e920f8959467db815d944ad9f7f9374ec9dc5db4
Philip Cooper <phillycooper@hotmail.com>
2020-03-24 14:56:40 +0000

    Update ModuleBuilder.cs
    Loging and conditional connection factory for overriding.

--- src/ModuleBuilder.cs
+++ src/ModuleBuilder.cs
@@ -22,11 +22,18 @@ namespace restlessmedia.Module
       containerBuilder.RegisterSettings<ICacheSettings>("restlessmedia/cache");
       #endregion
 
-      #region contexts
-      containerBuilder.RegisterType<ConnectionFactory>().As<IConnectionFactory>().SingleInstance();
+      // default log - trace
+      // we can add support for making this configurable so you could
+      // load another type to provide the implementation
+      containerBuilder.RegisterType<TraceLog>().As<ILog>();
+
+      // don't register the default connection factory if it's already been found and registered
+      // this gives other modules the opportunity to register their own to add pre execute calls per connection (set context for example)
+      containerBuilder.RegisterIf<ConnectionFactory, IConnectionFactory>(registry => !registry.IsRegistered<IConnectionFactory>())
+        .SingleInstance();
+
       containerBuilder.RegisterType<SqlRetry>().As<IRetry>().SingleInstance();
       containerBuilder.RegisterType<DataContext>().As<IDataContext>().SingleInstance();
-      #endregion
 
       #region services
       containerBuilder.RegisterType<EntityService>().As<IEntityService>().SingleInstance();
