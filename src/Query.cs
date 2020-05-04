6a527f2d8d54cf79f91384a85ab2c389fef4df3f
Philip Cooper <phillycooper@hotmail.com>
2020-05-04 19:11:19 +0100

    Added query.

---
+++ src/Query.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace restlessmedia.Module
+{
+  public class Query
+  {
+    public Query(int page, int maxPerPage)
+    {
+      Page = page;
+      MaxPerPage = maxPerPage;
+      Columns = new List<string>();
+    }
+
+    public Query()
+      : this(1, 10) { }
+
+    [Ignore]
+    public int Page { get; set; }
+
+    public virtual int MaxPerPage { get; set; }
+
+    public string Filter { get; set; }
+
+    public readonly IList<string> Columns;
+
+    [Ignore]
+    public int StartRow
+    {
+      get
+      {
+        return (Page - 1) * MaxPerPage;
+      }
+    }
+  }
+}
