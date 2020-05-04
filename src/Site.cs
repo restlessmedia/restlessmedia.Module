7d7088d8f4d0ccfc7e30502afd20bf3f3fb6b4d8
Philip Cooper <phillycooper@hotmail.com>
2020-05-04 21:42:02 +0100

    Models

---
+++ src/Site.cs
@@ -0,0 +1,14 @@
+﻿namespace restlessmedia.Module
+{
+  public class Site
+  {
+    public Site()
+    {
+      Active = true;
+    }
+
+    public Marker[] Markers { get; set; }
+
+    public bool Active { get; set; }
+  }
+}
