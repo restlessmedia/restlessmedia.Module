9c0fc59caacbfcd16f1f8a19a022b3e690c47aff
Philip Cooper <phillycooper@hotmail.com>
2020-05-10 11:05:57 +0100

    Linq extensions

---
+++ restlessmedia.Module.UnitTest/Extensions/LinqExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using restlessmedia.Test;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace restlessmedia.Module.UnitTest.Extensions
+{
+  public class LinqExtensionsTests
+  {
+    [Fact]
+    public void Page_tests()
+    {
+      IEnumerable<TestClass> testClasses = Enumerable.Repeat(new TestClass(), 15);
+
+      LinqExtensions.Page(testClasses, 1, 10).Count().MustBe(10);
+      LinqExtensions.Page(testClasses, 1, 5).Count().MustBe(5);
+      LinqExtensions.Page(testClasses, 3, 10).Count().MustBe(0);
+      LinqExtensions.Page(testClasses, 2, 10).Count().MustBe(5);
+    }
+
+    private class TestClass { }
+  }
+}
---
+++ src/Extensions/LinqExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace System.Collections.Generic
+{
+  public static class LinqExtensions
+  {
+    public static IEnumerable<T> Page<T>(this IEnumerable<T> source, int page, int maxPerPage)
+    {
+      if (source == null)
+      {
+        return null;
+      }
+
+      if (page == 1)
+      {
+        return source.Take(maxPerPage);
+      }
+
+      int skip = maxPerPage * (page - 1);
+
+      return source.Skip(skip).Take(maxPerPage);
+    }
+  }
+}
