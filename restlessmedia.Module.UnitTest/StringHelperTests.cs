2a4d22112d48e0f9b6c4740bbd7396d9b45971cc
Philip Cooper <phillycooper@hotmail.com>
2020-06-06 09:43:08 +0100

    Added string helper

---
+++ restlessmedia.Module.UnitTest/StringHelperTests.cs
@@ -0,0 +1,22 @@
+﻿using restlessmedia.Test;
+using Xunit;
+
+namespace restlessmedia.Module.UnitTest
+{
+  public class StringHelperTests
+  {
+    [Fact]
+    public void strips_out_xml_tags()
+    {
+      string html = "<strong>a test</strong>";
+      StringHelper.StripXmlTags(html).MustBe("a test");
+    }
+
+    [Fact]
+    public void strips_out_nested_xml_tags()
+    {
+      string html = "<strong>a test <span>nested</span></strong>";
+      StringHelper.StripXmlTags(html).MustBe("a test nested");
+    }
+  }
+}
---
+++ src/StringHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace restlessmedia.Module
+{
+  public class StringHelper
+  {
+    /// <summary>
+    /// Strips the xml tags revealing the inner text.
+    /// </summary>
+    /// <param name="html"></param>
+    /// <returns></returns>
+    public static string StripXmlTags(string html)
+    {
+      if (string.IsNullOrWhiteSpace(html))
+      {
+        return html;
+      }
+
+      const string _matchTagPattern = @"</?\w+((\s+\w+(\s*=\s*(?:"".*?""|'.*?'|[\^'"">\s]+))?)+\s*|\s*)/?>";
+      return Regex.Replace(html, _matchTagPattern, string.Empty).Trim();
+    }
+  }
+}
