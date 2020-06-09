30c460dae6b602397da9a4ddf4012aeb32e92bbd
Philip Cooper <phillycooper@hotmail.com>
2020-06-09 14:28:18 +0100

    Added configuration helper.

---
+++ src/Configuration/ConfigurationHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.Configuration;
+
+namespace restlessmedia.Module.Configuration
+{
+  public class ConfigurationHelper
+  {
+    public static T GetSection<T>(string path)
+    {
+      return GetSection<T>(path, true);
+    }
+
+    public static T GetSection<T>(string path, bool required)
+    {
+      if (!TryGetSection(path, out object section) && required)
+      {
+        throw new ConfigurationErrorsException($"Unable to find configuration section for {path}.");
+      }
+
+      return (T)section;
+    }
+
+    public static bool IsDefined(string path)
+    {
+      return ConfigurationManager.GetSection(path) != null;
+    }
+
+    public static bool TryGetSection(string path, out object section)
+    {
+      return (section = ConfigurationManager.GetSection(path)) != null;
+    }
+  }
+}
