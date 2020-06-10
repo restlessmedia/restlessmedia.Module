e69ec7dc72540744b374c2fdd310043e5add4aa1
Philip Cooper <phillycooper@hotmail.com>
2020-06-10 17:11:03 +0100

    Added license settings extensions

---
+++ src/Configuration/ILicenseSettingsExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace restlessmedia.Module.Configuration
+{
+  public static class ILicenseSettingsExtensions
+  {
+    public static bool HasFeature(this ILicenseSettings licenseSettings, string name)
+    {
+      return licenseSettings.FeatureCollection != null && licenseSettings.FeatureCollection.Any(x => x.Name == name);
+    }
+
+    public static bool IsEnabled(this ILicenseSettings licenseSettings, string name)
+    {
+      if (licenseSettings.FeatureCollection == null)
+      {
+        return false;
+      }
+
+      IFeature feature = licenseSettings.FeatureCollection.FirstOrDefault(x => string.Equals(x.Name, name, StringComparison.OrdinalIgnoreCase));
+
+      return feature != null && feature.Enabled;
+    }
+  }
+}
