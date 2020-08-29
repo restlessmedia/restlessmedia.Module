731b279a31530e92769f30cc94f1ff102594e03c
Philip Cooper <phillycooper@hotmail.com>
2020-08-29 09:22:53 +0100

    Exposing module finding routine for web

--- src/ModuleLoader.cs
+++ src/ModuleLoader.cs
@@ -22,36 +22,60 @@ namespace restlessmedia.Module
     /// <param name="assembly"></param>
     public static void Load(Action<TModule> factory, Assembly assembly = null)
     {
-      IEnumerable<Type> types = assembly != null ? FindModuleTypes(assembly) : FindModuleTypes();
-
-      foreach (Type moduleType in types)
+      if (assembly != null)
       {
-        TModule module = CreateModule(moduleType);
-        factory(module);
+        foreach (Type moduleType in GetModuleTypes(assembly))
+        {
+          factory(CreateModule(moduleType));
+        }
+      }
+      else
+      {
+        Load(factory, "*.module");
+      }
+    }
+
+    /// <summary>
+    /// Loads modules automatically, or for the given assembly.
+    /// </summary>
+    /// <param name="factory"></param>
+    /// <param name="pattern"></param>
+    public static void Load(Action<TModule> factory, string pattern)
+    {
+      foreach (Type moduleType in FindModuleTypes(pattern))
+      {
+        factory(CreateModule(moduleType));
       }
     }
 
     /// <summary>
     /// Finds module types by looking for .module files.
     /// </summary>
+    /// <param name="pattern"></param>
     /// <returns></returns>
-    private static IEnumerable<Type> FindModuleTypes()
+    public static IEnumerable<Type> FindModuleTypes(string pattern)
     {
       // look for .module files in the bin folder
       string executingDirectory = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase);
       executingDirectory = executingDirectory.Replace("file:\\", "");
-      return Directory.GetFiles(executingDirectory, "*.module").SelectMany(FindModuleTypes);
+      return Directory.GetFiles(executingDirectory, pattern)
+        .SelectMany(GetModuleTypes);
     }
 
-    private static IEnumerable<Type> FindModuleTypes(string pathToModuleFile)
+    /// <summary>
+    /// Returns module types based on the .module file.
+    /// </summary>
+    /// <param name="pathToModuleFile"></param>
+    /// <returns></returns>
+    private static IEnumerable<Type> GetModuleTypes(string pathToModuleFile)
     {
       string moduleFile = Path.GetFileName(pathToModuleFile);
       string assemblyName = moduleFile.Replace(".module", string.Empty);
       Assembly moduleAssembly = Assembly.Load(assemblyName);
-      return FindModuleTypes(moduleAssembly);
+      return GetModuleTypes(moduleAssembly);
     }
 
-    private static IEnumerable<Type> FindModuleTypes(Assembly assembly)
+    private static IEnumerable<Type> GetModuleTypes(Assembly assembly)
     {
       Type abstractModuleType = typeof(TModule);
       Trace.TraceInformation($"{abstractModuleType.FullName} module loader scanning {assembly.FullName} for {abstractModuleType.Name} types.");
