e538f99d567de5172866c6bc47df9420535e9df5
Philip Cooper <phillycooper@hotmail.com>
2020-11-17 10:11:05 +0000

    Adding connection factory for use with setting context.

---
+++ src/LicensedModuleConnectionFactory.cs
@@ -0,0 +1,28 @@
+﻿using restlessmedia.Module.Configuration;
+using restlessmedia.Module.Data;
+using System;
+using System.Data;
+
+namespace restlessmedia.Module
+{
+  public class LicensedModuleConnectionFactory : ModuleConnectionFactory
+  {
+    public LicensedModuleConnectionFactory(IDatabaseSettings databaseSettings, ILicenseSettings licenseSettings)
+      : base(databaseSettings)
+    {
+      _licenseSettings = licenseSettings ?? throw new ArgumentNullException(nameof(licenseSettings));
+    }
+
+    public override IDbConnection CreateConnection(bool open = false)
+    {
+      IDbConnection connection = base.CreateConnection(open);
+      if (open)
+      {
+        LicenseHelper.SetContext(connection, _licenseSettings);
+      }
+      return connection;
+    }
+
+    private readonly ILicenseSettings _licenseSettings;
+  }
+}
